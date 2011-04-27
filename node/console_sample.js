583daa9b7a669ac15cf281cf2002efa64e695bcd
Fumitaka Makino <fumitaka.makino@gmail.com>
2011-04-28 07:58:12 +0800

    まあ色々な練習コード

---
+++ node/console_sample.js
@@ -0,0 +1,6 @@
+console.time('100-elements');
+for (var i = 0; i < 100; i++) {
+;
+}
+console.timeEnd('100-elements');
+
---
+++ node/test.js
@@ -0,0 +1,6 @@
+var a='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+console.log(a.length);
+for (var i=0;i<a.length;i++){
+console.log(a.charAt(i));
+}
+
