3859f4ff8ce7d782d524de49ace3e15d2c719e23
Fumitaka Makino <fumitaka.makino@gmail.com>
2011-05-03 19:00:55 +0800

    タブをスペース2へ置換

--- python/building_smart_web_2.0_applications/recommendations.py
+++ python/building_smart_web_2.0_applications/recommendations.py
@@ -1,52 +1,52 @@
 
 # source data
 critics={
-'Lisa Rose':					{'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Lister': 3.0},
-'Gene Seymour':				{'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Lister': 3.0, 'You, Me and Dupree': 3.5},
-'Michael Phillips':		{'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0, 'Superman Returns': 3.5, 'The Night Lister': 4.0},
-'Claudia Puig':				{'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'The Night Lister': 4.5, 'Superman Returns': 4.0, 'You, Me and Dupree': 2.5},
-'Mick LaSalle':				{'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Lister': 3.0, 'You, Me and Dupree': 2.0},
-'Jack Matthews':			{'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'The Night Lister': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
-'Toby':								{'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0}
+'Lisa Rose':         {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Lister': 3.0},
+'Gene Seymour':      {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Lister': 3.0, 'You, Me and Dupree': 3.5},
+'Michael Phillips':  {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0, 'Superman Returns': 3.5, 'The Night Lister': 4.0},
+'Claudia Puig':      {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'The Night Lister': 4.5, 'Superman Returns': 4.0, 'You, Me and Dupree': 2.5},
+'Mick LaSalle':      {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Lister': 3.0, 'You, Me and Dupree': 2.0},
+'Jack Matthews':     {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'The Night Lister': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby':              {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0}
 }
 
 from math import sqrt
 
 def sim_distance(prefs, p1, p2):
-	si={}
-	for item in prefs[p1]:
-		if item in prefs[p2]:
-			si[item]=1
+  si={}
+  for item in prefs[p1]:
+    if item in prefs[p2]:
+      si[item]=1
 
-	if len(si)==0: return 0
-	
-	sum_of_squares=sum([pow(prefs[p1][item]-prefs[p2][item],2) for item in si]) 
-	
-	return 1/(1+sqrt(sum_of_squares))
+  if len(si)==0: return 0
+  
+  sum_of_squares=sum([pow(prefs[p1][item]-prefs[p2][item],2) for item in si]) 
+  
+  return 1/(1+sqrt(sum_of_squares))
 
 
 def sim_pearson(prefs, p1, p2):
-	si={}
-	for item in prefs[p1]:
-		if item in prefs[p2]: 
-			si[item]=1
-	
-	n=len(si)
-	if n==0: return 0
+  si={}
+  for item in prefs[p1]:
+    if item in prefs[p2]: 
+      si[item]=1
+  
+  n=len(si)
+  if n==0: return 0
 
-	sum1=sum([prefs[p1][it] for it in si])
-	sum2=sum([prefs[p2][it] for it in si])
-	
-	sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
-	sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
+  sum1=sum([prefs[p1][it] for it in si])
+  sum2=sum([prefs[p2][it] for it in si])
+  
+  sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
+  sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
 
-	pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+  pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
 
-	num=pSum-(sum1*sum2/n)
-	den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
-	if den==0: return 0
+  num=pSum-(sum1*sum2/n)
+  den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+  if den==0: return 0
 
-	r=num/den
+  r=num/den
 
-	return r
+  return r
 
