03376ced145dc2f88932e668c77c9b12811a31a9
Fumitaka Makino <fumitaka.makino@gmail.com>
2011-06-14 03:07:02 +0800

    sendとrecvのタイミングを実験

--- multiprocess/pipe.py
+++ multiprocess/pipe.py
@@ -9,7 +9,9 @@ def f(conn):
 
 if __name__=='__main__':
 	parent_conn, child_conn=Pipe()
-	p=Process(target=f, args=(chile_conn,))
+	p=Process(target=f, args=(child_conn,))
+	# send前にrecvを実行すると待機状態になるのでプロセスが停止する
+	#print parent_conn.recv()
 	p.start()
 	print parent_conn.recv()
 	p.join()
